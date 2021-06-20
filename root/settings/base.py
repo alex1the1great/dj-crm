60e811edb1fd83bf25bc5c902c74e81388d88cf8
Asim Shrestha <asimshrestha608@gmail.com>
2021-06-20 08:28:20 +0545

    Create leads app & config it

--- root/settings/base.py
+++ root/settings/base.py
@@ -40,6 +40,7 @@ THIRD_PARTY_APPS = []
 
 LOCAL_APPS = [
     'apps.accounts.apps.AccountsConfig',
+    'apps.leads.apps.LeadsConfig',
 ]
 INSTALLED_APPS = DJANGO_APPS + THIRD_PARTY_APPS + LOCAL_APPS
 
