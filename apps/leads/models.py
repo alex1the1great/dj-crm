e743be79849865bf59b1275010f4d77833cd760a
Asim Shrestha <asimshrestha608@gmail.com>
2021-06-20 09:07:11 +0545

    Create lead & agent model & migrate it

--- apps/leads/models.py
+++ apps/leads/models.py
@@ -1,3 +1,20 @@
 from django.db import models
+from django.contrib.auth import get_user_model
 
-# Create your models here.
+
+class Agent(models.Model):
+    user = models.OneToOneField(get_user_model(), on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.user.email
+
+
+class Lead(models.Model):
+    first_name = models.CharField(max_length=20)
+    last_name = models.CharField(max_length=20)
+    age = models.IntegerField(default=0)
+    agent = models.ForeignKey(Agent, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f'{self.first_name} {self.last_name}'
+
