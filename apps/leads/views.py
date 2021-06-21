0cb4de9333dd7bdcd55ca20df5750c30e612e6cb
Asim Shrestha <asimshrestha608@gmail.com>
2021-06-21 08:47:06 +0545

    Add landing page & update UI

--- apps/leads/views.py
+++ apps/leads/views.py
@@ -5,6 +5,10 @@ from .models import Lead, Agent
 from .forms import LeadForm
 
 
+def landing_page(request):
+    return render(request, 'landing.html')
+
+
 def lead_list(request):
     leads = Lead.objects.all()
     return render(request, 'leads/lead_list.html', {'leads': leads})
--- root/urls.py
+++ root/urls.py
@@ -2,8 +2,13 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 
+from apps.leads.views import landing_page
+
 urlpatterns = [
     path('admin/', admin.site.urls),
+
+    # Local
+    path('', landing_page, name='landing_page'),
     path('leads/', include('apps.leads.urls', namespace='leads'))
 ]
 
