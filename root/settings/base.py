4499e8e3759160ef19a59de0d59596cf82e56e47
Asim Shrestha <asimshrestha608@gmail.com>
2021-06-21 16:05:49 +0545

    Create pages app & config it

--- root/settings/base.py
+++ root/settings/base.py
@@ -41,6 +41,7 @@ THIRD_PARTY_APPS = []
 LOCAL_APPS = [
     'apps.accounts.apps.AccountsConfig',
     'apps.leads.apps.LeadsConfig',
+    'apps.pages.apps.PagesConfig',
 ]
 INSTALLED_APPS = DJANGO_APPS + THIRD_PARTY_APPS + LOCAL_APPS
 
