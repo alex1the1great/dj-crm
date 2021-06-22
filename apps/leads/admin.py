7c3a53ba31b21f7ba0cc4087df7f5b2734564b5d
Asim Shrestha <asimshrestha608@gmail.com>
2021-06-22 08:50:57 +0545

    Feat: create user profile using signal

--- apps/leads/admin.py
+++ apps/leads/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 
-from .models import Lead, Agent
+from .models import Lead, Agent, UserProfile
 
 admin.site.register(Lead)
 admin.site.register(Agent)
+admin.site.register(UserProfile)
